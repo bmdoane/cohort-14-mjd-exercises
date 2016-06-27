20477316f10e0097e71bc3fd55352fab0d253fc4
Brian Doane <brianmdoane@gmail.com>
2016-06-27 12:23:28 -0500

    saving changes, to work on another branch

--- acme-explosives/js/main.js
+++ acme-explosives/js/main.js
@@ -1,9 +1,9 @@
 // This function does one thing, and returns a promise
 var loadCategories = function() {
   return new Promise((resolve, reject) => {
-    $.ajax({
-      url: "json/categories.json"
-    }).done(function(data) {
+    $.ajax("json/categories.json")
+
+    .done(function(data) {
       resolve(data);
     }).fail(function(xhr, status, error) {
       reject(error);
@@ -12,13 +12,16 @@ var loadCategories = function() {
 };
 
 // This function does one thing, and returns a promise
+// Passing in result of firstXHR would be to build object
 var loadTypes = function(result_of_firstXHR) {
   return new Promise((resolve, reject) => {
-    $.ajax({
-      url: "json/types.json",
-      data: result_of_firstXHR
-    }).done(function(data) {
-      // create obj with first: second:
+    $.ajax("json/types.json")
+
+    .done(function(data) {
+      var acmeObj = {
+        jsonOne: result_of_firstXHR,
+        jsonTwo: data
+      }
       resolve(data);
     }).fail(function(xhr, status, error) {
       reject(error);
@@ -29,10 +32,12 @@ var loadTypes = function(result_of_firstXHR) {
 // This function does one thing, and returns a promise
 var loadProducts = function(result_of_secondXHR) {
   return new Promise((resolve, reject) => {
-    $.ajax({
-      url: "json/products.json",
-      data: result_of_secondXHR
-    }).done(function(data) {
+    $.ajax("json/products.json")
+
+    .done(function(data) {
+      var acmeObj = {
+        
+      }
       resolve(data);
     }).fail(function(xhr, status, error) {
       reject(error);
@@ -52,6 +57,7 @@ var catArray = [];
 
 loadCategories()
   .then(function(data1) {
+    console.log("data1", data1);
     catArray.push(data1.categories)
     return loadTypes(data1);
   })
@@ -64,3 +70,72 @@ loadCategories()
   });
 
 
+
+
+
+// // ORIGINAL CODE
+// // This function does one thing, and returns a promise
+// var loadCategories = function() {
+//   return new Promise((resolve, reject) => {
+//     $.ajax({
+//       url: "json/categories.json"
+//     }).done(function(data) {
+//       resolve(data);
+//     }).fail(function(xhr, status, error) {
+//       reject(error);
+//     });
+//   })
+// };
+
+// // This function does one thing, and returns a promise
+// var loadTypes = function(result_of_firstXHR) {
+//   return new Promise((resolve, reject) => {
+//     $.ajax({
+//       url: "json/types.json",
+//       data: result_of_firstXHR
+//     }).done(function(data) {
+//       // create obj with first: second:
+//       resolve(data);
+//     }).fail(function(xhr, status, error) {
+//       reject(error);
+//     });
+//   })
+// };
+
+// // This function does one thing, and returns a promise
+// var loadProducts = function(result_of_secondXHR) {
+//   return new Promise((resolve, reject) => {
+//     $.ajax({
+//       url: "json/products.json",
+//       data: result_of_secondXHR
+//     }).done(function(data) {
+//       resolve(data);
+//     }).fail(function(xhr, status, error) {
+//       reject(error);
+//     });
+//   })
+// };
+
+// /*
+//   Now we use those Promises to describe the order of execution, 
+//   and how data flows between each one.
+
+//   Note how the resolve callback function, itself, returns another
+//   promise. This is how you can chain promises, and dictate the
+//   order of execution of multiple aynschronous operations.
+//  */
+// var catArray = [];
+
+// loadCategories()
+//   .then(function(data1) {
+//     console.log("data1", data1);
+//     catArray.push(data1.categories)
+//     return loadTypes(data1);
+//   })
+//   .then(function(data2) {
+//    catArray.push(data2.types);
+//     return loadProducts(data2);
+//   }).then(function(data3){
+//     catArray.push(data3.products);
+//     console.log("catArray: ", catArray)
+//   });
