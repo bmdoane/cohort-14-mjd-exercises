a466e9b441d656b9eccbb13775809e3d93c742fa
Brian Doane <brianmdoane@gmail.com>
2016-07-01 14:08:52 -0500

    flattened json. products are on page.  need to be styled.

--- acme-explosives/js/eventListeners.js
+++ acme-explosives/js/eventListeners.js
@@ -1,8 +1,10 @@
-$(document).ready(function() {
+"use strict";
 
-	  // Handling click events
-	$("#explosives").change(function() {
-   	console.log("cluck");
-  });
-
+// Handling click events
+// Value represents value on select elements
+$("#explosives").change(function() {
+ 	selectedCategoryId = parseInt($('#explosives').val());
+ 	console.log("selectedCategoryId", selectedCategoryId);
+ 	loadData();
 });
+
