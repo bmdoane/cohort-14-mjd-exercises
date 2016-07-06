e84d822278b9e22472ef15c2f87bd9ccfec03166
Brian Doane <brianmdoane@gmail.com>
2016-07-05 21:59:52 -0500

    added index.html, main.js, main.css

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -1,2 +1,24 @@
 "use strict";
 console.log("hello nurse");
+
+// The user will enter a number in the text input.
+// The user will then press one of the buttons for an operation.
+// The input field should be cleared so that a new number can be typed in.
+// The user will then type in a new number and press enter.
+// The resulting value of the operation should then be in the text input.
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
