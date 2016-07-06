61575fd39b8ad809673488d6af5a8f76578e2487
Brian Doane <brianmdoane@gmail.com>
2016-07-05 23:46:38 -0500

    testing completed, started event listeners and dom functionality.  learning jquery

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -1,12 +1,6 @@
 "use strict";
-console.log("hello nurse");
 
-// The user will enter a number in the text input.
-// The user will then press one of the buttons for an operation.
-// The input field should be cleared so that a new number can be typed in.
-// The user will then type in a new number and press enter.
-// The resulting value of the operation should then be in the text input.
-
+// Jasmine tested calculator functions
 function add(a, b) {
   return a + b;
 }
@@ -21,4 +15,57 @@ function multiply(a, b) {
 
 function divide(a, b) {
 	return a / b;
+}
+
+function squared(a) {
+	return a * a;
+}
+
+function squareRoot(a) {
+	return Math.sqrt(a);
+}
+
+// The user will enter a number in the text input.
+// The user will then press one of the buttons for an operation.
+// The input field should be cleared so that a new number can be typed in.
+// The user will then type in a new number and press enter.
+// The resulting value of the operation should then be in the text input.
+
+let input = $('#user-input');
+
+$('#addition').click(function() {
+	// add(a, b);
+	console.log("Heyo");
+});
+
+$('#subtraction').click(function() {
+	// add(a, b);
+	console.log("Mio");
+});
+
+$('#multiplication').click(function() {
+	// add(a, b);
+	console.log("You");
+});
+
+$('#division').click(function() {
+	// add(a, b);
+	console.log("No");
+});
+
+$('#sqr').click(function() {
+	// add(a, b);
+	console.log("That guy");
+});
+
+$('#sqrt').click(function() {
+	// add(a, b);
+	console.log("Pete?");
+});
+
+// function calculate() {
+// 	if (input.val() === '') {
+
+// 	}
+
 }
--- calculator/spec/MathSpec.js
+++ calculator/spec/MathSpec.js
@@ -24,4 +24,28 @@ describe("The basic functions for a calculator", function() {
 		expect(multiply(10, 3)).toBe(30);
 	});
 
+	it("Should have a divide function", function() {
+		expect(divide).toBeDefined();
+	});
+
+	it("Should divide one integer with another", function() {
+		expect(divide(15, 3)).toBe(5);
+	});
+
+	it("Should have a squared function", function() {
+		expect(squared).toBeDefined();
+	});
+
+	it("Should square an integer", function() {
+		expect(squared(5)).toBe(25);
+	});
+
+	it("Should have a squareRoot function", function() {
+		expect(squareRoot).toBeDefined();
+	});
+
+	it("Should define the square root of an integer", function() {
+		expect(squareRoot(81)).toBe(9);
+	});
+
 });
